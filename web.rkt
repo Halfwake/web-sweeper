fd6ca7c4d5241168f342cca779396f89e9b3fb0b
Andrew <drewdudash@gmail.com>
2014-09-27 14:45:46 -0400

    lowered the mine rate

--- web.rkt
+++ web.rkt
@@ -10,37 +10,26 @@
 
 (struct game-state (grid))
 
-(define (render-cell a-grid x y embed/url)
+(define (reveal-link a-grid x y)
+  (λ (req)
+    (cell-reveal-propogate a-grid x y)))
+
+(define (render-cell a-grid x y)
   (define a-cell (grid-ref a-grid x y))
-  (cond [(cell-hidden? a-cell)
-         `(a ([href ,(embed/url
-                      (λ (req)
-                        (cell-reveal-propogate a-grid x y)))])
-             "X")]
-        [(and (not (cell-hidden? a-cell))
-              (cell-mine? a-cell))
-         "*"]
-        [(and (not (cell-hidden? a-cell))
-              (not (cell-mine? a-cell)))
-         (format "~a" (count-adjacent-mines a-grid x y))]))
+  (cond [(cell-hidden? a-cell) "X"]
+        [(cell-mine? a-cell) "*"]
+        [else (format "~a" (count-adjacent-mines a-grid x y))]))
 
-
-(define (render-cells a-grid embed/url)
-  (grid-map (λ (iter-value iter-x iter-y)
-              `(td (render-cell a-grid iter-x iter-y embed/url)))))
-
-(define (render-row a-row)
-  `(tr ,@a-row))
-
-(define (chunkify lst n)
-  (if (null? lst)
-      '()
-      (cons (take n lst) (chunkify (drop n lst) n))))
 
 (define (render-grid a-grid embed/url)
-  `(table ,@(map render-row (chunkify (render-cells a-grid embed/url) (grid-width a-grid)))))
-
-
+  (for/list ([y (grid-height a-grid)])
+    `(tr ,@(for/list ([x (grid-width a-grid)])
+             `(td ,(cond [(cell-hidden? (grid-ref a-grid x y))
+                          `(a ([href ,(embed/url  (reveal-link a-grid x y))])
+                              ,(render-cell a-grid x y))]
+                         [else (render-cell a-grid x y)]))))))
+  
+  
 ;; TODO Refactor
 (define (play-game a-game-state)
   (match-define (game-state a-grid) a-game-state)
@@ -51,23 +40,11 @@
        (response/xexpr
         `(html (head (title "MineSweeper"))
                (body (h1 "MineSweeper")
-                     (table
-                      ,@(for/list ([y (grid-height a-grid)])
-                          `(tr ,@(for/list ([x (grid-width a-grid)])
-                                  `(td ,(cond [(cell-hidden? (grid-ref a-grid x y))
-                                             `(a ([href ,(embed/url
-                                                          (λ (req)
-                                                            (cell-reveal-propogate a-grid x y)))])
-                                                 "X")]
-                                            [(and (not (cell-hidden? (grid-ref a-grid x y)))
-                                                  (cell-mine? (grid-ref a-grid x y)))
-                                             "*"]
-                                            [(and (not (cell-hidden? (grid-ref a-grid x y)))
-                                                  (not (cell-mine? (grid-ref a-grid x y))))
-                                             (format "~a" (count-adjacent-mines a-grid x y))]))))))))))))))
+                     (table ,@(render-grid a-grid embed/url))))))))))
+                      
 
 (define (new-game req)
-  (play-game (game-state (make-minefield 10 10 .2))))
+  (play-game (game-state (make-minefield 10 10 .05))))
 
 (module+ main
   (serve/servlet new-game))
