f750951a5918551245adf9b4e4ececa6612a8a53
Andrew <drewdudash@gmail.com>
2014-09-30 18:22:06 -0400

    add win lose prompts newgame button and new header

--- web.rkt
+++ web.rkt
@@ -33,6 +33,13 @@
                           7 "/7.png"
                           8 "/8.png"))
 
+(define game-images (hash 'title "/title.png"
+                          'new-game "/new_game.png"
+                          'instructions "/instructions.png"
+                          'winner "/winner.png"
+                          'loser "/loser.png"))
+                          
+
 (define (render-cell-image a-grid x y)
   (define a-cell (grid-ref a-grid x y))
   `(img ([src ,(cond [(cell-hidden? a-cell) (hash-ref cell-images 'hidden)]
@@ -52,6 +59,36 @@
                                         ,(render-cell a-grid x y render-mode))]
                                    [else (render-cell a-grid x y render-mode)])))))))
 
+(define (new-game-link)
+  (λ (req)
+    (minefield-spawner)))
+
+(define (render-new-game render-mode embed/url)
+  `(a ([href ,(embed/url (new-game-link))])
+      ,(match render-mode
+         ['text "New Game"]
+         ['image `(img ([src ,(hash-ref game-images 'new-game)]))])))
+                      
+(define (render-title render-mode)
+  (match render-mode
+    ['text "Minesweeper"]
+    ['image `(img ([src ,(hash-ref game-images 'title)]))]))
+
+(define (render-state-text a-grid)
+  (cond [(won-game? a-grid) "You Won"]
+        [(lost-game? a-grid) "You Lost"]))
+
+(define (render-state-image a-grid)
+  `(img ([src ,(cond [(won-game? a-grid) (hash-ref game-images 'winner)]
+                     [(lost-game? a-grid) (hash-ref game-images 'loser)]
+                     [else (hash-ref game-images 'instructions)])])))
+                      
+
+(define (render-state a-grid render-mode)
+  (match render-mode
+    ['text (render-state-text a-grid)]
+    ['image (render-state-image a-grid)]))
+
 (define (play-game a-game-state)
   (match-define (game-state a-grid render-mode) a-game-state)
   (play-game
@@ -60,12 +97,18 @@
      (λ (embed/url)
        (response/xexpr
         `(html (head (title "MineSweeper"))
-               (body (h1 "MineSweeper")
-                     ,(render-grid a-grid render-mode embed/url))))))
+               (body ,(render-title render-mode)
+                     ,(render-grid a-grid render-mode embed/url)
+                     ,(render-state a-grid render-mode)
+                     (br)
+                     ,(render-new-game render-mode embed/url))))))
     render-mode)))
+
+(define (minefield-spawner)
+  (make-minefield 10 10 .05))
                       
 (define (new-game req)
-  (play-game (game-state (make-minefield 10 10 .05) 'image)))
+  (play-game (game-state (minefield-spawner) 'image)))
 
 (module+ main
   (serve/servlet new-game #:extra-files-paths (list "./images/") #:stateless? #t))
